2d44289ee64ee1ceb3601a68d0e01863e0815317
Edwin <edwinradvilavicius@yahoo.com>
2022-01-18 14:45:52 +0000

    Refactored variable and function names to increase readability

--- face_detection.py
+++ face_detection.py
@@ -5,19 +5,19 @@ import cv2
 class FaceDetection:
     # Initializing the following attributes: MTCNN, video capture and the image path
     def __init__(self):
-        self.face_detector = MTCNN()
+        self.mtcnn = MTCNN()
         self.capture = cv2.VideoCapture(0)
         self.face_image_path = "C:/Users/Edwin/OneDrive - University of Greenwich/Year 3/Final Year " \
                                "Project/Programming/Facial-Recognition/cropped_faces"
 
-    def face_detection(self):
+    def face_detector(self):
         crop_counter = 0
         while True:
             # Capture each frame
             __, frame = self.capture.read()
 
             # Store the frame with the detected faces
-            result = self.face_detector.detect_faces(frame)
+            result = self.mtcnn.detect_faces(frame)
             if result:
                 for person in result:
                     # Store the location of the box and key points
@@ -27,8 +27,8 @@ class FaceDetection:
                     # Slice and store the image to only include the ROI
                     roi_cropped = frame[int(bounding_box[1]):
                                         int(bounding_box[1] + bounding_box[3]),
-                                  int(bounding_box[0]):
-                                  int(bounding_box[0] + bounding_box[2])]
+                                        int(bounding_box[0]):
+                                        int(bounding_box[0] + bounding_box[2])]
 
                     # Display a window showing the cropped face
                     cv2.imshow("ROI", roi_cropped)
@@ -54,6 +54,7 @@ class FaceDetection:
 
             # Display the frame with the detected faces
             cv2.imshow('frame', frame)
+
             # If q is pressed stop the capture
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -64,8 +65,8 @@ class FaceDetection:
 
 
 def main():
-    face_detector = FaceDetection()
-    face_detector.face_detection()
+    face_detection = FaceDetection()
+    face_detection.face_detector()
 
 
 if __name__ == "__main__":
