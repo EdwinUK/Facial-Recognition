6cef9b054f89622dd26589559841528716013ab7
Edwin <edwinradvilavicius@yahoo.com>
2022-02-05 16:38:12 +0000

    Optimizied the face detection class

--- face_detection.py
+++ face_detection.py
@@ -18,53 +18,12 @@ class FaceDetection:
         self.capture = cv2.VideoCapture(0)
         self.root_path = os.path.dirname(os.path.abspath("face_detection.py"))
         self.face_image_path = os.path.join(self.root_path, "cropped_faces")
+        self.input_image_path = os.path.join(self.root_path, "application_data", "input_image", "input_image.jpg")
 
     def face_detector(self):
         while True:
             # Capture each frame
             __, frame = self.capture.read()
-
-            # Store the frame with the detected faces
-            result = self.mtcnn.detect_faces(frame)
-            if result:
-                for person in result:
-                    # Store the location of the box and key points
-                    bounding_box = person['box']
-                    landmarks = person['keypoints']
-
-                    # Slice and store the image to only include the ROI
-                    roi_cropped = frame[int(bounding_box[1]):
-                                        int(bounding_box[1] + bounding_box[3]),
-                                        int(bounding_box[0]):
-                                        int(bounding_box[0] + bounding_box[2])]
-
-                    # if s is pressed then resize the cropped ROI and save it locally
-                    if cv2.waitKey(1) & 0xFF == ord('s'):
-                        roi_cropped = cv2.resize(roi_cropped, (250, 250))
-                        cv2.imwrite(f"{self.face_image_path}/{uuid.uuid1()}.jpg", roi_cropped)
-                        print("ROI image captured")
-
-                    # if v is pressed then save the cropped roi and attempt verification
-                    # and if the 0.5 verification threshold is met then print that the user has been verified
-                    if cv2.waitKey(10) & 0xFF == ord('v'):
-                        cv2.imwrite(os.path.join("application_data", "input_image", "input_image.jpg"), roi_cropped)
-                        face_recognition = FaceRecognition()
-                        results, verified = face_recognition.verification(0.5, 0.5)
-                        print("You have been verified!" if verified else "Access denied!")
-
-                    # Draw a bounding box around the face
-                    cv2.rectangle(frame,
-                                  (bounding_box[0], bounding_box[1]),
-                                  (bounding_box[0] + bounding_box[2], bounding_box[1] + bounding_box[3]),
-                                  (204, 51, 255),
-                                  2)
-
-                    # Draw circles around facial landmarks
-                    cv2.circle(frame, (landmarks['left_eye']), 2, (0, 255, 0), 2)
-                    cv2.circle(frame, (landmarks['right_eye']), 2, (0, 255, 0), 2)
-                    cv2.circle(frame, (landmarks['nose']), 2, (0, 255, 0), 2)
-                    cv2.circle(frame, (landmarks['mouth_left']), 2, (0, 255, 0), 2)
-                    cv2.circle(frame, (landmarks['mouth_right']), 2, (0, 255, 0), 2)
 
             # Display the frame with the detected faces
             cv2.imshow('Face Recognition', frame)
@@ -72,6 +31,39 @@ class FaceDetection:
             # If q is pressed stop the capture
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
+            # If s is pressed then resize the cropped ROI and save it locally
+            # if cv2.waitKey(1) & 0xFF == ord('s'):
+            #     roi_cropped = cv2.resize(roi_cropped, (250, 250))
+            #     cv2.imwrite(f"{self.face_image_path}/{uuid.uuid1()}.jpg", roi_cropped)
+            #     print("ROI image captured")
+
+            # If v is pressed then save the frame locally and let MTCNN read it
+            if cv2.waitKey(10) & 0xFF == ord('v'):
+                cv2.imwrite(self.input_image_path, frame)
+                pixels = cv2.imread(self.input_image_path)
+                result = self.mtcnn.detect_faces(pixels)
+
+                # Only process the image if there is 1 face in the frame
+                if len(result) == 1:
+                    for person in result:
+                        # Store the location of the box and key points
+                        bounding_box = person['box']
+                        landmarks = person['keypoints']
+
+                        # Slice and store the image to only include the ROI
+                        roi_cropped = frame[int(bounding_box[1]):
+                                            int(bounding_box[1] + bounding_box[3]),
+                                            int(bounding_box[0]):
+                                            int(bounding_box[0] + bounding_box[2])]
+                        cv2.imwrite(self.input_image_path, roi_cropped)
+
+                        # If the 0.5 verification threshold is met then print that the user has been verified
+                        face_recognition = FaceRecognition()
+                        results, verified = face_recognition.verification(0.5, 0.9)
+                        print("You have been verified!" if verified else "Access denied!")
+                else:
+                    print("Too many faces in the image")
 
         # Release the use of the capture and destroy the window
         self.capture.release()
--- face_recognition.py
+++ face_recognition.py
@@ -38,7 +38,7 @@ class FaceRecognition:
         # Verification threshold is the amount of positive predictions divided by positive samples
         verification = detection / len(os.listdir(os.path.join("application_data", "verification_images")))
         verified = verification > verification_threshold
-        print(sum(results))
         print(results)
+        print(verification)
 
         return results, verified
