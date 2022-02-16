007acc13fc32f13a35d387bc1c405bc086976dc7
Edwin <edwinradvilavicius@yahoo.com>
2022-02-16 13:40:46 +0000

    Changed the model and code structure from Siamese to MobileNet

--- face_recognition.py
+++ face_recognition.py
@@ -2,43 +2,27 @@ import os
 import numpy as np
 import tensorflow as tf
 
-from distance_layer import L1Distance
 
-
-# Preprocessing function to prepare data for the model
-def siamese_model_preprocess(file_path):
+# Preprocessing function to prepare input for the mobilenet model
+def mobilenet_preprocess(file_path):
     img_in_bytes = tf.io.read_file(file_path)
     img = tf.io.decode_jpeg(img_in_bytes)
-    img = tf.image.resize(img, (100, 100))
-    img = img / 255.0
+    img = tf.image.resize(img, (224, 224))
     return img
 
 
 class FaceRecognition:
+    # Initialising the saved mobilenetv2 model
     def __init__(self):
-        self.siamese_model = \
-            tf.keras.models.load_model("cnn_models/siamesemodel.h5",
-                                       custom_objects={"L1Distance": L1Distance,
-                                                       "BinaryCrossentropy": tf.losses.BinaryCrossentropy},
-                                       compile=False)
+        self.mobilenet_model = tf.keras.models.load_model("cnn_models/mobilenetv2.h5")
 
-    def face_verification(self, detection_threshold, verification_threshold):
-        # Create results array and preprocess input and validation data
-        results = []
-        for image in os.listdir(os.path.join("application_images", "verification_images")):
-            input_image = siamese_model_preprocess(os.path.join("application_images", "input_image", "input_image.jpg"))
-            validation_image = siamese_model_preprocess(os.path.join("application_images", "verification_images", image))
+    def face_verification(self):
+        # Preprocessing the input image then passing the input to the model to make a prediction
+        input_image = mobilenet_preprocess(os.path.join("application_images", "input_image", "input_image.jpg"))
+        prediction = self.mobilenet_model.predict(np.expand_dims(input_image, axis=0))
 
-            # Make predictions on the data and append to the results array
-            result = self.siamese_model.predict(list(np.expand_dims([input_image, validation_image], axis=1)))
-            results.append(result)
+        # Converting the prediction to a 0 or 1 value
+        prediction = tf.nn.sigmoid(prediction)
+        prediction = tf.where(prediction < 0.5, 0, 1)
 
-        # Detection threshold is the metric used to determine whether a prediction is considered positive
-        detection = np.sum(np.array(results) > detection_threshold)
-
-        # Verification threshold is the amount of positive predictions divided by positive samples
-        verification = detection / len(os.listdir(os.path.join("application_images", "verification_images")))
-        print(results)
-        verified = verification > verification_threshold
-
-        return verified
+        return prediction.numpy()[0][0]
---
+++ main.py
@@ -0,0 +1,88 @@
+import cv2
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.logger import Logger
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+from kivy.core.window import Window
+
+from face_detection import FaceDetection
+from face_recognition import FaceRecognition
+from spoof_detection import SpoofDetection
+
+
+class MobileApp(App):
+    # Calling the parent class constructor and creating some app widget attributes
+    def __init__(self):
+        super().__init__()
+        self.app_title = Label(text="Facial Recognition", size_hint=(1, .1), font_size='20sp', bold=True)
+        self.webcam = Image(size_hint=(1, .7))
+        self.button = Button(text="Verify!", on_press=self.verify_user, size_hint=(1, .1), color=[0, 0, 0, 1],
+                             background_color=[255, 255, 255, 1])
+        self.verified_status = Label(text="Verification waiting to start!", size_hint=(1, .1))
+        self.capture = cv2.VideoCapture(0)
+
+    # Building the app
+    def build(self):
+        # Creating a box layout and adding the widget attributes to it
+        layout = BoxLayout(orientation="vertical")
+        layout.add_widget(self.app_title)
+        layout.add_widget(self.webcam)
+        layout.add_widget(self.verified_status)
+        layout.add_widget(self.button)
+        Window.size = (250, 450)
+
+        # Schedule the update_capture function to run continuously
+        Clock.schedule_interval(self.update_capture, 1.0 / 33.0)
+
+        return layout
+
+    # Updating the webcam feed continuously
+    def update_capture(self, *args):
+        # Capture each frame and set the frame size to 250x250
+        __, frame = self.capture.read()
+        frame = frame[120:120 + 250, 200:200 + 250, :]
+
+        # Flip the frame horizontal and convert the image to a texture
+        buf = cv2.flip(frame, 0).tobytes()
+        img_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt="bgr")
+        img_texture.blit_buffer(buf, colorfmt="bgr", bufferfmt="ubyte")
+        self.webcam.texture = img_texture
+
+    # The function which calls of the main classes to perform face detection, spoof detection and face recognition
+    def verify_user(self, *args):
+        face_detection = FaceDetection()
+        __, frame = self.capture.read()
+        frame = frame[120:120 + 250, 200:200 + 250, :]
+
+        cv2.imwrite(face_detection.input_image_path, frame)
+        amount_of_faces = face_detection.face_detector()
+
+        if amount_of_faces == 1:
+            # Call the spoof detector method which will detect whether the input is a spoof attack
+            spoof_detection = SpoofDetection()
+            spoof_result = spoof_detection.spoof_detector()
+
+            # If the spoof_result is 1 then it's real input, if it's 0 then it's a spoof attack
+            if spoof_result == 1:
+                # Create an instance of the face recognition class and call the verification function
+                # If the 0.5 verification threshold is met then print that the user has been verified
+                face_recognition = FaceRecognition()
+                verified = face_recognition.face_verification()
+                self.verified_status.text = "You have been verified!" if verified == 1 else "You have not been " \
+                                                                                            "recognised! "
+                self.verified_status.color = "green" if verified == 1 else "red"
+            else:
+                self.verified_status.text = "Spoof attack detected!"
+                self.verified_status.color = "red"
+        else:
+            self.verified_status.text = amount_of_faces
+            self.verified_status.color = "red"
+
+
+if __name__ == "__main__":
+    MobileApp().run()
