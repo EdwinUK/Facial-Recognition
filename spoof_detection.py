d7349ef3e1e5ed68c404f419f1f71c6c4714336d
Edwin <edwinradvilavicius@yahoo.com>
2022-02-16 14:17:39 +0000

    Removed a print statement

--- spoof_detection.py
+++ spoof_detection.py
@@ -9,7 +9,6 @@ class SpoofDetection:
         self.efficientnet_model = tf.keras.models.load_model("cnn_models/efficientnetb0.h5")
 
     def spoof_detector(self, input_image):
-        print(input_image)
         # Passing the input to the model to make a prediction
         prediction = self.efficientnet_model.predict(np.expand_dims(input_image, axis=0))
 
